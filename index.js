433ec4fe7b6bcad1095a3d1111d830093b6f7a95
Daniel <65896341+dclif@users.noreply.github.com>
2022-03-10 15:49:33 +0000

    conditional logic
    For each function looping through input with conditional logic to provide relevant output

--- index.js
+++ index.js
@@ -2,5 +2,35 @@ function marsRover(URCoords, startPos, directions){
         const compass = ['N','E','S','W']
         let direction = compass.indexOf(startPos[2])
         let position = [startPos[0], startPos[1]];
+
+        directions.split('').forEach((element) => { 
         
+            if(element === "L" && direction > 0){
+                direction--;
+            }
+            else if(element === "L" && direction === 0){
+                direction = 3;
+            }
+            else if(element === "R" && direction < 3){
+                direction++;
+            }
+            else if(element === "R" && direction === 3){
+                direction = 0;
+            }
+            else if(element === "M" && direction === 1){
+                position[0]++;
+            }
+            else if(element === "M" && direction === 3){
+                position[0]--;
+            }
+            else if(element === "M" && direction === 0){
+                position[1]++;
+            }
+            else if(element === "M" && direction === 2){
+                position[1]--;
+            }
+            
+         });
+         return [...position, compass[direction]].toString();
+
 }

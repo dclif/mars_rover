c0aea794c378471cebd1baa161b988118b9e2ea4
Daniel <65896341+dclif@users.noreply.github.com>
2022-03-10 15:48:17 +0000

    declare variables

---
+++ index.js
@@ -0,0 +1,6 @@
+function marsRover(URCoords, startPos, directions){
+        const compass = ['N','E','S','W']
+        let direction = compass.indexOf(startPos[2])
+        let position = [startPos[0], startPos[1]];
+        
+}

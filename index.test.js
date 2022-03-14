136db6e503789de1ff01dc3047989a2c552c9c3e
Daniel <65896341+dclif@users.noreply.github.com>
2022-03-14 16:58:13 +0000

    Add multiple rover capabilities. Added new tests.

--- index.test.js
+++ index.test.js
@@ -8,15 +8,28 @@ test('Check rover turns right', () => {
     expect(marsRover('55','12N','R')).toEqual('1 2 E\n');
 });
 
-test('Check gives correct answer for  Left, Right and Forward combinations', () => {
+test('Check rover moves forward', () => {
+    expect(marsRover('55','12N','M')).toEqual('1 3 N\n');
+});
+
+test('Check accepts two rovers', () => {
+    expect(marsRover('55','12N','M', '33E', 'M')).toEqual('1 3 N\n4 3 E\n');
+});
+
+test('Check gives correct answer for  Left, Right and Forward combinations with two rovers', () => {
     expect(marsRover('55','12N','LMLMLMLMM', '33E', 'MMRMMRMRRM')).toEqual('1 3 N\n5 1 E\n');
 });
 
-test('Check rover will not over run grid boundaries', () => {
+test('Check rovers will not over run grid boundaries', () => {
     expect(marsRover('55','12N','LMMMMMMLMMMMMM', '33E', 'MMRRRMMM')).toEqual('0 0 S\n5 5 N\n');
 });
 
 test('Check will throw error if paramaters are missing', () => {
     expect(() => {marsRover('55','12N','LMMMMMMLMMMMMM', '33E')}).toThrow();
 });
+
+test('Check accepts three rovers', () => {
+    expect(marsRover('55','12N','LMMMMMMLMMMMMM', '33E', 'MMRRRMMM', '00E', 'MLMRM')).toEqual('0 0 S\n5 5 N\n2 1 E\n');
+});
+
 
